5dacc2f0fa1ee611ae9c0034bd9cee29aa33b596
NelsonRs <nelsonribera1@gmail.com>
2023-02-05 01:06:08 -0400

    creacion del proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    $root = $_SERVER["DOCUMENT_ROOT"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio | HellBurger</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body> 
+    <button id="theme-switch">Change</button>
+    
+<script src="<?php $root?>assets/js/main.js"></script>
+</body>
+</html>
