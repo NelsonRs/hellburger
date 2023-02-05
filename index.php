57be4c7d1fde916e8b1b79fab799a921730bc768
NelsonRs <nelsonribera1@gmail.com>
2023-02-05 02:10:19 -0400

    se añadio el index y se agrego el diseño

--- index.php
+++ index.php
@@ -12,7 +12,38 @@
 </head>
 <body> 
     <button id="theme-switch">Change</button>
-    
+    <section class="section-index">
+        <header>
+            <h1>Productos</h1>
+        </header>
+        <section class="section-hamburguer">
+            <header>
+                <h2>Hamburguesas</h2>
+            </header>
+            <div class="card">
+                <img src="/assets/svg/burger.svg">
+                <div class="title">
+                    <h3>Burger Simple</h3>
+                    <p>Carne, Tocino, queso cheddar</p>
+                </div>
+                <div class="actions">
+                    <button>+</button>
+                    <button>-</button>
+                </div>
+            </div>
+            <div class="card">
+                <img src="/assets/svg/burger.svg">
+                <div class="title">
+                    <h3>Burger Simple</h3>
+                    <p>Carne, Tocino, queso cheddar</p>
+                </div>
+                <div class="actions">
+                    <button>+</button>
+                    <button>-</button>
+                </div>
+            </div>
+        </section>
+    </section>
 <script src="<?php $root?>assets/js/main.js"></script>
 </body>
 </html>
